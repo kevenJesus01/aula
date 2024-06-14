25aa202261431988e9bc90a73a6624d47aff188a
kevenjesus01001@gmail.com <kevenJesus01>
2024-06-13 21:33:08 -0300

    gostei

---
+++ aula-imput.py
@@ -0,0 +1,10 @@
+nome = input("digite seu nome: ")
+idade = input("digite sua idade: ")
+print(idade)
+# print(type(idade))
+# idade_furura = idade + 3 
+
+
+
+# print(f"O nome da pessoa é {nome}, e a idade é {idade} , idade futura {idade_futura}")
+
