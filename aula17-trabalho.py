a0cc64d1c9257f2f73a8943c8df108ee7d35d842
kevenjesus01001@gmail.com <kevenJesus01>
2024-06-17 19:19:25 -0300

    deu errado kkk

---
+++ aula17-trabalho.py
@@ -0,0 +1,8 @@
+nome = input("digite seu nome:")
+altura = input (input("digite a sua altura em metros"))
+idade = int (input("Digite a sua idade: "))
+
+
+
+print(f"Seu nome é {nome}, e sua altura é {altura} e sua idade é {idade}")
+
