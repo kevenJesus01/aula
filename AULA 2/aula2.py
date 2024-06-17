13c54d453864065131374a9200f15f34761bf7df
kevenjesus01001@gmail.com <kevenJesus01>
2024-06-17 20:27:25 -0300

    Soma-de-numeros

---
+++ AULA 2/aula2.py
@@ -0,0 +1,10 @@
+nome = input("digite seu nome: ")
+
+n1 = int(input("Digite sua nota: "))
+n2 = int(input("Digite sua nota: "))
+n3 = int(input("Digite sua nota: "))
+n4 = int(input("Digite sua nota: "))
+
+media = (n1 , n2 , n3 , n4) /4
+
+print(f"Olá, {nome} sua media é: {media} ")
